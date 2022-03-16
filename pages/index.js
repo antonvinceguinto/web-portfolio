38776f6d28229c509af850da07f3f1973f1d7bca
Anton Guinto <antonvinceguinto@gmail.com>
2022-03-16 19:35:21 +0800

    move introduction to a new component

--- pages/index.js
+++ pages/index.js
@@ -3,54 +3,9 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { Spacer } from '@nextui-org/react';
 import Navbar from '../components/Navbar';
-import BusinessCard from '../components/BusinessCard';
+import Introduction from '../components/Introduction';
 
 export default function Home() {
-  const introduction = (
-    <div className='lg:flex justify-around items-center lg:px-56 px-10'>
-      <div className='items-start lg:text-left flex-col'>
-        <div className='text-[1.9rem] lg:text-[3rem] font-bold flex-1 flex-col flex text-start lg:items-start'>
-          <div>
-            Im a <span className='text-gradient'>Software Developer</span>
-          </div>
-          <div>
-            I use <span className='text-gradient'>React & Nextjs</span>
-          </div>
-          <div>
-            and <span className='text-gradient'>Solidity.</span>
-          </div>
-        </div>
-        <Spacer y={0.9} />
-        <div className='text-gray-400'>
-          Dapp | Web3 content | Learning Blockchain👨‍💻.
-        </div>
-      </div>
-      <Spacer y={2} />
-      <Spacer x={3} />
-      <div className='sm:grid lg:grid-cols-2 gap-3'>
-        <BusinessCard
-          username='@antonguinto'
-          url='https://twitter.com/antonguinto'
-          hasFollowers={true}
-          footer='Twitter'
-        />
-        <BusinessCard
-          username='@guinto'
-          url='https://www.linkedin.com/in/guinto/'
-          image='/images/linkedin-me.jpg'
-          buttonLabel='Connect'
-          footer='Linkedin'
-        />
-        <BusinessCard
-          username='@antonvinceguinto'
-          url='https://github.com/antonvinceguinto'
-          image='/images/github-me.jpg'
-          footer='Github'
-        />
-      </div>
-    </div>
-  );
-
   return (
     <React.Fragment>
       <Head>
@@ -74,7 +29,7 @@ export default function Home() {
           <Navbar />
           <div className='items-center justify-center flex-col flex'>
             <div className='flex-col text-center items-center justify-center mt-7 md:mt-24'>
-              {introduction}
+              <Introduction/>
             </div>
           </div>
         </div>
